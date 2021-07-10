f37f282fb145c7ac794948405e329c85bcbb14b1
Mayank-Shekhar09 <mayank.shekhar@ltts.com>
2021-07-10 17:19:03 +0530

    Add project files.

---
+++ InstructorBL/Instructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstructorBL
+{
+    public class Instructor
+    {
+        private float avgFeedback;
+        private int experience;
+        private string instructorName;
+        private string[] instructorSkill;
+
+        public Instructor()
+        {
+
+        }
+
+        public Instructor(string instructorName, float avgFeedback, int experience, string[] instructorSkill)
+        {
+            this.instructorName = instructorName;
+            this.avgFeedback = avgFeedback;
+            this.experience = experience;
+            this.instructorSkill = instructorSkill;
+        }
+
+        static bool CheckSkill(string technology)
+        {
+
+        }
+
+        public bool validateEligibility()
+        {
+
+        }
+
+    }
+}
---
+++ InstructorUI/Instructor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InstructorUI
+{
+    class Instructor
+    {
+        static string[] InstructorSkill = new string[10000];
+        static int Experience;
+        static float AvgFeedback;
+
+        static void Main(string[] args)
+        {
+        /* float avgFeedback;
+         int experience;
+         string instructorName;
+         string[] instructorSkill = new string[Int32.MaxValue];*/
+
+
+        AvgFeedback = (float)Convert.ToDouble(Console.ReadLine());
+        Experience = Convert.ToInt32(Console.ReadLine());
+        string InstructorName = Console.ReadLine();
+            
+        int input = Convert.ToInt32(Console.ReadLine());
+        for(int index = 0; index < input;index++){
+            InstructorSkill[index] = Console.ReadLine();
+            }
+        Instructor inst = new Instructor(InstructorName, AvgFeedback, Experience, InstructorSkill);
+        string technology = Console.ReadLine();
+        bool hasSkill = inst.CheckSkill(technology);
+            if (hasSkill)
+                Console.WriteLine("Match Found");
+            else
+                Console.WriteLine("Match not Found");
+    }
+
+    public Instructor()
+        {
+
+        }
+
+    Instructor(string instructorName, float avgFeedback, int experience, string[] instructorSkill)
+    {
+    instructorName = instructorName;
+    avgFeedback = avgFeedback;
+    experience = experience;
+    instructorSkill = instructorSkill;
+    }
+
+    bool CheckSkill(string technology)
+    {
+            int pos = -1;
+        for(int index = 0; index < InstructorSkill.Length; index++)
+            {
+                if (technology.Equals(InstructorSkill[index]))
+                    pos = index;
+            }
+        if (pos == -1)
+            return false;
+        else
+        {
+            return ValidateEligibility();
+        }
+    }
+
+    public bool ValidateEligibility()
+    {
+        if(Experience>3 && AvgFeedback >= 4.5)
+        {
+            return true;
+        }
+        else if(Experience<3 && AvgFeedback >= 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+}
